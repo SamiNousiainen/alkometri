fe3e9577096083d4ff91b96d989a47cc96a8ffbe
SamiNousiainen <n1nosa00@students.oamk.fi>
2022-02-04 18:15:51 +0200

    alkometri korjauksia + css 04022022

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [gender, setGender] = useState("male")
   const [result, setResult] = useState(0)
 
-  const calc = (e) => {
+  function calc (e) {
     e.preventDefault();
     const litres = bottles * 0.33;
     const grams = litres * 8 * 4.5;
@@ -28,18 +28,19 @@ function App() {
     }
     setResult(promille);
   }
-  
+
   return (
     <form onSubmit={calc}>
       <h3>Calculating blood alcohol level</h3>
       <div>
-        <label>Weight: </label>
+        <label>Weight:</label>
         <input type="number" value={weight} onChange={e => setWeight(e.target.value)}/>
       </div>
 
       <div>
-        <label>Bottles: </label>
+        <label>Bottles:</label>
         <select value={bottles} onChange={e => setBottles(e.target.value)}>
+          <option value="0">0</option>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -54,8 +55,9 @@ function App() {
       </div>
 
       <div>
-        <label>Time: </label>
+        <label>Time:</label>
         <select value={time} onChange={e => setTime(e.target.value)}>
+          <option value="0">0</option>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -70,7 +72,7 @@ function App() {
       </div>
 
       <div>
-        <label>Gender: </label>
+        <label>Gender</label>
         <select value={gender} onChange={e => setGender(e.target.value)}>
           <option value="male">male</option>
           <option value="female">female</option>
@@ -78,9 +80,9 @@ function App() {
       </div>
 
       <div>
-        <output> {result}</output>
+        <output> {result.toFixed(2)}</output>
       </div>
-    <button>Calculate</button>
+    <button>calculate</button>
 
     </form>
   )
